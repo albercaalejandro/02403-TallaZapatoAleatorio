b3b1383245de556228bf75c6fd26dbe5d3e45168
Xtek <Xtek@Pc-Nasa>
2019-10-25 18:33:19 +0200

    :tada: Commit inicial

---
+++ src/tallazapatoaleatorio/Main.java
@@ -0,0 +1,32 @@
+/* 
+ * Copyright 2019 Alejandro Alberca - alejandro.alberca.alum@iescamp.es.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tallazapatoaleatorio;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Alejandro Alberca - alejandro.alberca.alum@iescamp.es
+ */
+public class Main {
+    public static final Random RND = new Random();
+    public static void main(String[] args) {
+        int max=60;
+        int min=20;
+        int talla = RND.nextInt(max-min)+min;
+        System.out.println("La talla es la siguiente: "+talla);
+    }
+}
